2a41eb5a56eac8427bc5148cfccc4e4239085b2c
Cedric Niguidula <cedcn-9mail@yahoo.com>
2017-10-04 03:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function theBeatlesPlay (musicians,  instruments) {
   var array = [];
-  for (i=0;  i <= musicians.length ; i++) {
+  for (i=0;  i < musicians.length ; i++) {
     array.push (musicians[i] + " plays " + instruments[i] + ".");
   }
-  console.log(array);
   return array;
 }
